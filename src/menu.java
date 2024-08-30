db2e3e2fc150afcc4d895fd023404726b0c3b303
unknown <ismail.ouali@ump.ac.ma>
2024-08-30 11:26:10 +0100

    Role menu

---
+++ src/menu.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+
+public class menu {
+    public static void main(String[] args) {
+        
+        int selectedChoice;
+        do{
+            selectedChoice = roleMenu();
+            switch (selectedChoice) {
+                case 1:
+                    menuAdmin();
+                    break;
+                case 2:
+                    System.out.println("You are now participant");
+                    break;
+                default:
+                    break;
+            }
+        }
+        while(selectedChoice > 2);
+
+    }
+
+    public static  int roleMenu()
+    {
+        
+        int selection;
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("**********************\n");
+        System.out.println("Select Your Role\n");
+        System.out.println("(1) Admin\n");
+        System.out.println("(2) Participant\n");
+        System.out.println("**********************\n");
+
+        System.out.println("Your selection is: ");
+        selection = sc.nextInt();
+
+        return selection;
+    }
+
+    public static int menuAdmin()
+    {
+        int choice;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*****Admin Menu******\n");
+        System.out.println("(1) Crud an event\n");
+        System.out.println("(2) Crud of Participant \n");
+        System.out.println("(3) Show all inscriptions \n");
+        System.out.println("**********************\n");
+
+        choice = sc.nextInt();   
+        return choice; 
+
+    }
+}
